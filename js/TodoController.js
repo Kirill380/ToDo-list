2a2a080d72beb1d52c72046c8f3b6ed539977148
Kirill Liubun <kreol_123@ukr.net>
2015-12-28 00:25:50 +0200

    Small fix with styles and added item-add, item mark done and item remove functionality

--- js/TodoController.js
+++ js/TodoController.js
@@ -1,7 +1,61 @@
-/**
- * Created by Nout on 27/12/2015.
- */
-(function($) {
 
+(function($) {
+    $(document).ready(function() {
+        var container = $(".todo-list");
+        var addBtn = container.find(".jsAdd");
+        var addField = container.find(".jsAddField");
+        var head = container.find(".jsHead");
+        var items;
+
+        //TODO use jade
+        var content = '<div class="todo-list__item">\
+                       <div class="item">\
+                            <div class="item__checkbox">\
+                                <input type="checkbox"/>\
+                            </div>\
+                            <div class="item__content">\
+                                <%= title%>\
+                            </div>\
+                            <div class="item__remove">\
+                                <span class="fa fa-times-circle-o"></span>\
+                            </div>\
+                        </div>\
+                    </div>';
+
+        var tpl = _.template(content);
+
+        addBtn.on("click", function () {
+            var item = addField.val();
+
+            if(_.isEmpty(item)) {
+                console.log("nothing to add");
+                return;
+            }
+
+            var html = tpl({title : item});
+            head.after(html);
+            head.removeClass("jsHead");
+            head = head.next();
+            head.addClass("jsHead");
+
+            //add event handlers
+            head.find(".item__remove").on("click", function () {
+                var $item = $(this).parent().parent();
+                if($item.hasClass("jsHead")) {
+                    head = $item.prev();
+                    head.addClass("jsHead");
+                }
+                $item.remove();
+            });
+
+            head.find(".item__checkbox").on("change", function() {
+                var $item = $(this).parent();
+                $item.find(".item__content").toggleClass("done-item", this.checked);
+            });
+
+        });
+
+
+    });
 
 })(jQuery);
