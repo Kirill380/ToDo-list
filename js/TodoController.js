f21c88753ff1345290a782c39657d08d578173a4
Kirill Liubun <kreol_123@ukr.net>
2015-12-28 01:11:12 +0200

    fix with styles and added search form

--- js/TodoController.js
+++ js/TodoController.js
@@ -1,11 +1,10 @@
-
 (function($) {
     $(document).ready(function() {
         var container = $(".todo-list");
         var addBtn = container.find(".jsAdd");
         var addField = container.find(".jsAddField");
         var head = container.find(".jsHead");
-        var items;
+        var search = container.find(".jsFieldSearch");
 
         //TODO use jade
         var content = '<div class="todo-list__item">\
@@ -25,7 +24,18 @@
         var tpl = _.template(content);
 
         addBtn.on("click", function () {
+            addNewItem();
+        });
+
+        addField.on("keypress", function(e) {
+            if(e.keyCode == 13 )
+                addNewItem();
+        });
+
+
+        function addNewItem() {
             var item = addField.val();
+            addField.val("");
 
             if(_.isEmpty(item)) {
                 console.log("nothing to add");
@@ -40,6 +50,7 @@
 
             //add event handlers
             head.find(".item__remove").on("click", function () {
+                //TODO think about better solution
                 var $item = $(this).parent().parent();
                 if($item.hasClass("jsHead")) {
                     head = $item.prev();
@@ -52,10 +63,23 @@
                 var $item = $(this).parent();
                 $item.find(".item__content").toggleClass("done-item", this.checked);
             });
+        }
 
+        search.on("keydown", function(e) {
+            var word = this.value;
+            var items = container.find(".todo-list__item:not(.todo-list__item_control)");
+
+            items.find(".item__content").each(function(i, content) {
+                var $content = $(content);
+
+                if($content.parent().parent().attr('id') == "init")
+                    return;
+
+                if($content.text().indexOf(word) == -1)
+                    $content.parent().parent().addClass("hidden");
+                else
+                    $content.parent().parent().removeClass("hidden");
+            })
         });
-
-
     });
-
 })(jQuery);
